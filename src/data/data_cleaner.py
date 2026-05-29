1bb33a8d5fec84625aeda7410bc1eec0fc3748ef
tamim1982 <readulislamtamim@gmail.com>
2026-05-29 15:59:46 +0600

    Phase 4, Step 3 is done

--- src/data/data_cleaner.py
+++ src/data/data_cleaner.py
@@ -30,6 +30,8 @@ if PROJECT_ROOT not in sys.path:
 
 #%%
 
+import pandas as pd
+
 class DataCleaner:
     def __init__(self, dataframe):
         self.dataframe = dataframe
@@ -43,4 +45,11 @@ class DataCleaner:
             self.dataframe = self.dataframe.dropna()
         elif strategy == "fill":
             self.dataframe = self.dataframe.fillna(fill_value)
+        return self.dataframe
+    
+    def convert_types(self):
+        self.dataframe["Date"] = pd.to_datetime(self.dataframe["Date"])
+
+        self.dataframe["StateHoliday"] = self.dataframe["StateHoliday"].astype("category")
+
         return self.dataframe
